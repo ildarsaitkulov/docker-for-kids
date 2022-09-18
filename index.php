3d0ab1bb66c558cdf2fb1258c6b4713ee380b1c0
saitkulov <saitkulovim@gmail.com>
2022-09-18 19:48:46 +0300

    +index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 
-$pdo = new \PDO("pgsql:host=postgres;dbname=postgres", 'postgres', 'mysecretpass');
-var_dump($pdo);
+try {
+    $pdo = new \PDO("pgsql:host=postgres;dbname=postgres", 'postgres', 'mysecretpass');
+    echo "Подключение к базе данных установлено! <br>";
+
+    return;
+} catch (PDOException $exception) {
+    echo "Ошибка при подключении к базе данных<br><b>{$exception->getMessage()}</b><br>";
+}
